8293b3c9a10339c760d2d4e1f3e928a34f7caf9a
Janu shankar <janakiharishankar24@gmail.com>
2022-01-13 18:50:22 +0530

    commit

---
+++ ReverseNo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class ReverseNo
+    {
+        public static void ReverseNumber()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter a number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.Write("Reversed Number: " + reverse);
+        }
+    }
+}
+    
+
