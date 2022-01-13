86d0548253f1bf11b39ffce3f73867048e685154
Janu shankar <janakiharishankar24@gmail.com>
2022-01-13 18:42:28 +0530

    second commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LogicalProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //   FibanocciSeries Uc1 = new FibanocciSeries();
+            // FibanocciSeries.Fibanocci();
+            //  Perfectno uc2 = new Perfectno();
+            //  Perfectno.PerfectNumber();
+            //  PrimeNumber uc3 = new PrimeNumber();
+            //  PrimeNumber.CheckPrime();
+            ReverseNo uc4 = new ReverseNo();
+            ReverseNo.ReverseNumber();
+            //  Couponno uc5 = new Couponno();
+            //  Couponno.PrintCouponno();
+            //  Stopwatch Uc6 = new Stopwatch();
+            // Stopwatch.CountTimer();
+
+        }
+    }
+}
