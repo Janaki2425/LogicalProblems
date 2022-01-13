616443eb10e32b4bdfd18c0caec93305ebef78d8
Janu shankar <janakiharishankar24@gmail.com>
2022-01-13 18:55:07 +0530

    last commit

---
+++ Couponno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class Couponno
+    {
+		public static void PrintCouponCode()
+		{
+			var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+			var stringChars = new char[6];
+			var random = new Random();
+
+			for (int i = 0; i < stringChars.Length; i++)
+			{
+				stringChars[i] = chars[random.Next(chars.Length)];
+			}
+
+
+		}
+	}
+
+}
+
+    
+
