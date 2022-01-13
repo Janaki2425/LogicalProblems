088e5ac91820c3d695ed21792eb40898006c9312
Janu shankar <janakiharishankar24@gmail.com>
2022-01-13 18:51:27 +0530

    last commit

---
+++ Stopwatch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class Stopwatch
+    {
+        public static void CountTimer()
+        {
+            SWatch sw = new SWatch();
+            sw.Start();
+            for (int i = 0; i < 1000; i++)
+            {
+                Thread.Sleep(1000);
+            }
+            sw.Stop();
+            Console.WriteLine("Time" + sw.Elapsed);
+        }
+    }
+}
+    }
+}
