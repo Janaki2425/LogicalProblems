33899e18deece4bd086d8fde1b70ec93f629c226
Janu shankar <janakiharishankar24@gmail.com>
2022-01-13 18:45:47 +0530

    commit

---
+++ FibonacciSeries.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class FibonacciSeries
+    {
+        public static void Fibonacci()
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.Write("Enter the number of elements: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");    
+            for (i = 2; i < number; ++i)   
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+
+        }
+    }
+ }
+
+
+    
+
+
+    
+}
